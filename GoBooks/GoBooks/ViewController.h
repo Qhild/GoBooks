935ab328584c9410becef355983c2b389b7bf98f
Q <826306707@qq.com>
2016-04-16 14:20:19 +0800

    新建初始工程

---
+++ GoBooks/GoBooks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoBooks
+//
+//  Created by Q on 16/4/16.
+//  Copyright © 2016年 GBSeventh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GoBooks/GoBooks/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GoBooks
+//
+//  Created by Q on 16/4/16.
+//  Copyright © 2016年 GBSeventh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
