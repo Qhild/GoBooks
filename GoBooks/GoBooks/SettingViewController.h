843e37a0a3040ec680ed1b30c11986784dcad410
xu <735296830@qq.com>
2016-04-18 17:40:14 +0800

    1

---
+++ GoBooks/GoBooks/SettingViewController.h
@@ -0,0 +1,14 @@
+//
+//  SettingViewController.h
+//  GoBooks
+//
+//  Created by xu on 16/4/18.
+//  Copyright © 2016年 GBSeventh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+- (IBAction)backAction:(UIBarButtonItem *)sender;
+
+@end
