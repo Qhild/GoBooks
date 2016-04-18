fb86752181db3dd9e0a891182c65ea24b868631f
Q <826306707@qq.com>
2016-04-18 16:13:45 +0800

    1

--- GoBooks/GoBooks/HomeViewController.h
+++ GoBooks/GoBooks/HomeViewController.h
@@ -11,5 +11,9 @@
 @interface HomeViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UISearchBar *searchFiled;
 @property (weak, nonatomic) IBOutlet UIScrollView *CarouselView;
+- (IBAction)homebtn1:(UIButton *)sender forEvent:(UIEvent *)event;
+- (IBAction)homebtn2:(UIButton *)sender forEvent:(UIEvent *)event;
+- (IBAction)homebtn3:(UIButton *)sender forEvent:(UIEvent *)event;
+- (IBAction)homebtn4:(UIButton *)sender forEvent:(UIEvent *)event;
 
 @end
