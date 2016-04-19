3197f48bdc183085011ddf9d08b37de976e1f878
Q <826306707@qq.com>
2016-04-19 18:58:44 +0800

    1

---
+++ GoBooks/GoBooks/bookDetailTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  bookDetailTableViewController.h
+//  GoBooks
+//
+//  Created by Q on 16/4/19.
+//  Copyright © 2016年 GBSeventh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface bookDetailTableViewController : UITableViewController
+
+@end
---
+++ GoBooks/GoBooks/htcTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  htcTableViewController.h
+//  GoBooks
+//
+//  Created by Q on 16/4/19.
+//  Copyright © 2016年 GBSeventh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface htcTableViewController : UITableViewController
+
+@end
