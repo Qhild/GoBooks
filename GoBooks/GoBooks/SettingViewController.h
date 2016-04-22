6b8073596fd7916d8ac2cb8be201474233b396ae
xu <735296830@qq.com>
2016-04-22 14:24:59 +0800

    gengxin

--- GoBooks/GoBooks/SettingViewController.h
+++ GoBooks/GoBooks/SettingViewController.h
@@ -2,15 +2,15 @@
 //  SettingViewController.h
 //  GoBooks
 //
-//  Created by xu on 16/4/19.
+//  Created by xu on 16/4/22.
 //  Copyright © 2016年 GBSeventh. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface SettingViewController : UIViewController
-- (IBAction)touxiangAction:(UIButton *)sender forEvent:(UIEvent *)event;
-@property (weak, nonatomic) IBOutlet UIImageView *photoIV;
 - (IBAction)pickAction:(UITapGestureRecognizer *)sender;
+@property (weak, nonatomic) IBOutlet UIImageView *photoIV;
+- (IBAction)portraitAction:(UIButton *)sender forEvent:(UIEvent *)event;
 
 @end
