28d64414b31e0bb03c942e887f6fd8314dcaa4a8
xu <735296830@qq.com>
2016-04-22 10:36:51 +0800

    更改找回密码的关联文件名

---
+++ GoBooks/GoBooks/RetrieveViewController.m
@@ -0,0 +1,37 @@
+//
+//  RetrieveViewController.m
+//  GoBooks
+//
+//  Created by xu on 16/4/22.
+//  Copyright © 2016年 GBSeventh. All rights reserved.
+//
+
+#import "RetrieveViewController.h"
+
+@interface RetrieveViewController ()
+
+@end
+
+@implementation RetrieveViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
