a27a1c162689018496c22f65833dd7bd737a8d37
Q <826306707@qq.com>
2016-04-23 17:20:27 +0800

    1

---
+++ GoBooks/GoBooks/HomeTableViewController.h
@@ -0,0 +1,20 @@
+//
+//  HomeTableViewController.h
+//  GoBooks
+//
+//  Created by Q on 16/4/23.
+//  Copyright © 2016年 GBSeventh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeTableViewController : UITableViewController
+@property (weak, nonatomic) IBOutlet UISearchBar *searchFiled;
+@property (weak, nonatomic) IBOutlet UIScrollView *CarouselView;
+- (IBAction)homebtn1:(UIButton *)sender forEvent:(UIEvent *)event;
+- (IBAction)homebtn2:(UIButton *)sender forEvent:(UIEvent *)event;
+- (IBAction)homebtn3:(UIButton *)sender forEvent:(UIEvent *)event;
+- (IBAction)homebtn4:(UIButton *)sender forEvent:(UIEvent *)event;
+@property (strong, nonatomic) IBOutlet UITableView *homeTableView;
+
+@end
