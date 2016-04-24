be3a6c74e4659b7166cbdece623e6a6f1d2d7549
xu <735296830@qq.com>
2016-04-24 17:00:49 +0800

    no message

---
+++ GoBooks/GoBooks/CollectionViewController.h
@@ -0,0 +1,14 @@
+//
+//  CollectionViewController.h
+//  GoBooks
+//
+//  Created by xu on 16/4/24.
+//  Copyright © 2016年 GBSeventh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewController : UICollectionViewController
+
+
+@end
