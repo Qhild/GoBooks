eb96dde7a1073247bedede4a41d7ae0fca252a3b
xu <735296830@qq.com>
2016-04-26 14:21:16 +0800

    geengxin

---
+++ GoBooks/GoBooks/CollectionTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  CollectionTableViewController.h
+//  GoBooks
+//
+//  Created by xu on 16/4/26.
+//  Copyright © 2016年 GBSeventh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionTableViewController : UITableViewController
+- (IBAction)deleteAction:(UIBarButtonItem *)sender;
+
+@end
